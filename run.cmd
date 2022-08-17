8098ac76830ff16e66fbafc6600dfb5debc00e31
Gagandeep <gagandeep@Gagandeep.local>
2022-08-18 00:51:12 +0530

    command files

---
+++ run.cmd
@@ -0,0 +1,8 @@
+# python -u to disable buffering
+# https://stackoverflow.com/questions/43197518/print-statements-not-working-when-serve-forever-is-called/43304567#43304567
+# Run websocket server
+nohup ./venv/bin/python3 -u websocket-server/ws_server.py 2>&1 > logs/ws-$(date '+%Y-%m-%dT%H:%M:%S').log &
+
+# Run auth redirect server
+nohup ./venv/bin/python3 -u auth-redirect-server/auth_redirect_server.py 2>&1 > logs/auth-$(date '+%Y-%m-%dT%H:%M:%S').log &
+
