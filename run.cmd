817d23c219db0d321e6a95671c8c44f9016485be
Gagandeep <gagandeep@Gagandeep.local>
2022-08-18 16:45:33 +0530

    moving ws_server and auth_redirect_server inside the main package

--- run.cmd
+++ run.cmd
@@ -1,8 +1,9 @@
 # python -u to disable buffering
 # https://stackoverflow.com/questions/43197518/print-statements-not-working-when-serve-forever-is-called/43304567#43304567
+
 # Run websocket server
-nohup ./venv/bin/python3 -u websocket-server/ws_server.py 2>&1 > logs/ws-$(date '+%Y-%m-%dT%H:%M:%S').log &
+nohup ./venv/bin/python3 -u streamlit_ws_localstorage/websocket-server/ws_server.py 2>&1 > logs/ws-$(date '+%Y-%m-%dT%H:%M:%S').log &
 
 # Run auth redirect server
-nohup ./venv/bin/python3 -u auth-redirect-server/auth_redirect_server.py 2>&1 > logs/auth-$(date '+%Y-%m-%dT%H:%M:%S').log &
+nohup ./venv/bin/python3 -u streamlit_ws_localstorage/auth-redirect-server/auth_redirect_server.py 2>&1 > logs/auth-$(date '+%Y-%m-%dT%H:%M:%S').log &
 
--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='streamlit_ws_localstorage',
     packages=setuptools.find_packages(),
-    version='1.0.1',  # Ideally should be same as your GitHub release tag varsion
+    version='1.0.2',  # Ideally should be same as your GitHub release tag varsion
     description="A simple synchronous way of accessing localStorage from your Streamlit app.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Gagandeep Singh',
     author_email='gagan@heloprotocol.in',
     url='https://github.com/gagangoku/streamlit-ws-localstorage',
-    download_url='https://github.com/gagangoku/streamlit-ws-localstorage/archive/refs/tags/1.0.1.tar.gz',
+    download_url='https://github.com/gagangoku/streamlit-ws-localstorage/archive/refs/tags/1.0.2.tar.gz',
     keywords=['python', 'websocket', 'cookies', 'localstorage', 'streamlit'],
     classifiers=[],
     python_requires=">=3.6",
