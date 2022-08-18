b7b1c0afe5555eb5eda11d6d5104b58f4d1b841a
Gagandeep <gagandeep@Gagandeep.local>
2022-08-18 17:05:10 +0530

    updating scripts

--- run.cmd
+++ run.cmd
@@ -2,8 +2,8 @@
 # https://stackoverflow.com/questions/43197518/print-statements-not-working-when-serve-forever-is-called/43304567#43304567
 
 # Run websocket server
-nohup ./venv/bin/python3 -u streamlit_ws_localstorage/websocket-server/ws_server.py 2>&1 > logs/ws-$(date '+%Y-%m-%dT%H:%M:%S').log &
+nohup ./venv/bin/python3 -u streamlit_ws_localstorage/websocket_server/ws_server.py 2>&1 > logs/ws-$(date '+%Y-%m-%dT%H:%M:%S').log &
 
 # Run auth redirect server
-nohup ./venv/bin/python3 -u streamlit_ws_localstorage/auth-redirect-server/auth_redirect_server.py 2>&1 > logs/auth-$(date '+%Y-%m-%dT%H:%M:%S').log &
+nohup ./venv/bin/python3 -u streamlit_ws_localstorage/auth_redirect_server/auth_redirect_server.py 2>&1 > logs/auth-$(date '+%Y-%m-%dT%H:%M:%S').log &
 
